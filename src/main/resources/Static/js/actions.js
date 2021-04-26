c4f08f2f1b83ffd72ebb3211bc296c36ac4f649e
Saten <saten.hovakimyan55@gmail.com>
2021-04-26 18:20:46 +0400

    Adding navbar icons

--- src/main/resources/Static/js/actions.js
+++ src/main/resources/Static/js/actions.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var options = {
-
+        coverTrigger:false,
+        alignment:'center',
     };
     var instances = M.Dropdown.init(elems, options);
 });
